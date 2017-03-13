73ec95abd8fec6280462362f860b9f44c15c7205
rpathur <rpathur@ncsu.edu>
2017-03-13 18:42:18 -0400

    test hello packet

--- HelloReplyPacket.py
+++ HelloReplyPacket.py
@@ -29,14 +29,15 @@ from netifaces import *
 def toBytes(x):
     return (int(x,16))
 
-def helloPacket(interface):
+def helloReplyPacket(interface,d_mac,dstip):
     if interface not in ni.interfaces():
         return None
 
     S_MAC = ni.ifaddresses(interface)[AF_LINK][0]['addr'].split(":")
     S_MAC = pack('!6c',chr(int(S_MAC[0], 16)), chr(int(S_MAC[1],16)),  chr(int(S_MAC[2],16)) , chr(int(S_MAC[3],16)), chr(int(S_MAC[4],16)), chr(int(S_MAC[5],16)))
-    D_MAC = pack('!6c',chr(int('ff', 16)), chr(int('ff',16)),  chr(int('ff',16)) , chr(int('ff',16)), chr(int('ff',16)), chr(int('ff',16)))
+    D_MAC = d_mac
     ETH_TYPE = pack('!2c',chr(int('08',16)),chr(int('00',16)))
+
 
     ethernet_header = S_MAC+D_MAC+ETH_TYPE  # 14 bytes
 
@@ -53,12 +54,12 @@ def helloPacket(interface):
     Src_IP = ni.ifaddresses(interface)[ni.AF_INET][0]['addr'].split('.')
 
     ip_src = pack('!4c',chr(int(Src_IP[0])),chr(int(Src_IP[1])),chr(int(Src_IP[2])),chr(int(Src_IP[3])))
-    ip_dst = pack('!4c',chr(224),chr(0),chr(0),chr(4))
+    ip_dst = dstip
 
 
     ip_header = ip_v_ihl + ip_tos + ip_tot_len + ip_id + ip_frag_off + ip_ttl + ip_proto + ip_check + ip_src + ip_dst
 
-    payload = pack('!c',chr(1))
+    payload = pack('!c',chr(2))
 
     return ethernet_header+ip_header+payload
 
@@ -66,11 +67,11 @@ def helloPacket(interface):
 def main():
 
     interface = sys.argv[1]
+    d_mac = pack('!6c',chr(int('ff', 16)), chr(int('ff',16)),  chr(int('ff',16)) , chr(int('ff',16)), chr(int('ff',16)), chr(int('ff',16)))
+    ip_dst = pack('!4c',chr(224),chr(0),chr(0),chr(4))
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
     s.bind((interface,0))
-    s.send(helloPacket(interface))
-
+    s.send(helloReplyPacket(interface,d_mac,ip_dst))
 
 if __name__ == "__main__":
     main()
-
