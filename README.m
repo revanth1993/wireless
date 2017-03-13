dd466ec7021d57bed293b9131df6478084388118
rpathur <rpathur@ncsu.edu>
2017-03-13 18:48:32 -0400

    test hello packet

--- README.m
+++ README.m
@@ -1,1 +1,10 @@
 # wireless
+
+Install netifaces using pip module
+
+Run sudo python HelloPacket.py interfacename  to send a sample hello packet
+use wireshark to capture the packet and observe the fields
+
+
+
+
