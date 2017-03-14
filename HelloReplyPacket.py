436ff831e07745fa38ec72831ee67c07cf6e2dd7
rpathur <rpathur@ncsu.edu>
2017-03-14 12:15:37 -0400

    test hello packet

--- HelloReplyPacket.py
+++ HelloReplyPacket.py
@@ -35,7 +35,10 @@ def helloReplyPacket(interface,d_mac,dstip):
 
     S_MAC = ni.ifaddresses(interface)[AF_LINK][0]['addr'].split(":")
     S_MAC = pack('!6c',chr(int(S_MAC[0], 16)), chr(int(S_MAC[1],16)),  chr(int(S_MAC[2],16)) , chr(int(S_MAC[3],16)), chr(int(S_MAC[4],16)), chr(int(S_MAC[5],16)))
-    D_MAC = d_mac
+
+    D_MAC = d_mac.split(':')
+    D_MAC = pack('!6c',chr(int(D_MAC[0], 16)), chr(int(D_MAC[1],16)),  chr(int(D_MAC[2],16)) , chr(int(D_MAC[3],16)), chr(int(D_MAC[4],16)), chr(int(D_MAC[5],16)))
+
     ETH_TYPE = pack('!2c',chr(int('08',16)),chr(int('00',16)))
 
 
@@ -54,7 +57,9 @@ def helloReplyPacket(interface,d_mac,dstip):
     Src_IP = ni.ifaddresses(interface)[ni.AF_INET][0]['addr'].split('.')
 
     ip_src = pack('!4c',chr(int(Src_IP[0])),chr(int(Src_IP[1])),chr(int(Src_IP[2])),chr(int(Src_IP[3])))
-    ip_dst = dstip
+
+    Dst_IP = dstip.split('.')
+    ip_dst = pack('!4c',chr(int(Dst_IP[0])),chr(int(Dst_IP[1])),chr(int(Dst_IP[2])),chr(int(Dst_IP[3])))
 
 
     ip_header = ip_v_ihl + ip_tos + ip_tot_len + ip_id + ip_frag_off + ip_ttl + ip_proto + ip_check + ip_src + ip_dst
