8a6912c712bd5bf99069b67a4429ac2953dc27ae
rpathur <rpathur@ncsu.edu>
2017-03-14 11:53:58 -0400

    test hello packet

--- ExtractPacket.py
+++ ExtractPacket.py
@@ -9,7 +9,7 @@ def extractPacketFields(packet):
     eth_type = hex(ord(packet[12]))+hex(ord(packet[13]))[2:]
 
 
-    src_ip = ord(packet[26])+'.'+ord(packet[27])+'.'+ord(packet[28])+'.'+ord(packet[29])
-    dst_ip = ord(packet[30])+'.'+ord(packet[31])+'.'+ord(packet[32])+'.'+ord(packet[33])
+    src_ip = str(ord(packet[26]))+'.'+str(ord(packet[27]))+'.'+str(ord(packet[28]))+'.'+str(ord(packet[29]))
+    dst_ip = str(ord(packet[30]))+'.'+str(ord(packet[31]))+'.'+str(ord(packet[32]))+'.'+str(ord(packet[33]))
     dsdv_type = ord(packet[34])
     return src_mac,dst_mac,eth_type,src_ip,dst_ip,dsdv_type
