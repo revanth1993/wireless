b93ec0cc45453ed9131c33dda0b3a91345e8d8e2
rpathur <rpathur@ncsu.edu>
2017-03-14 11:51:02 -0400

    test hello packet

--- ExtractPacket.py
+++ ExtractPacket.py
@@ -6,7 +6,7 @@ def extractPacketFields(packet):
 
     src_mac = hex(ord(packet[0]))[2:]+':'+hex(ord(packet[1]))[2:]+':'+hex(ord(packet[2]))[2:]+':'+hex(ord(packet[3]))[2:]+':'+hex(ord(packet[4]))[2:]+':'+hex(ord(packet[5]))[2:]
     dst_mac = hex(ord(packet[6]))[2:]+':'+hex(ord(packet[7]))[2:]+':'+hex(ord(packet[8]))[2:]+':'+hex(ord(packet[9]))[2:]+':'+hex(ord(packet[10]))[2:]+':'+hex(ord(packet[11]))[2:]
-    eth_type = hex(packet[12])+hex(packet[13])[2:]
+    eth_type = hex(ord(packet[12]))+hex(ord(packet[13]))[2:]
 
 
     src_ip = ord(packet[26])+'.'+ord(packet[27])+'.'+ord(packet[28])+'.'+ord(packet[29])
