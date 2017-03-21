d2a24bc58501eba9923d3a4762743ed6f47cd0e3
rpathur <rpathur@ncsu.edu>
2017-03-21 11:13:09 -0400

    included extract dd packet

--- ExtractPacket.py
+++ ExtractPacket.py
@@ -25,7 +25,10 @@ def extractPacketFields(packet):
         start = 38
 
         for entry in xrange(entries):
-            rib_neighbor[getIP(packet[start:start+4])] = [int(packet[start+5]+packet[start+6],16),int(packet[start+7]+packet[start+8],16)]
+            ip = getIP(packet[start:start+4])
+            delay = ord(packet[start+5])*100 + ord(packet[start+6])
+            sequence_number = ord(packet[start+7])*100 + ord(packet[start+8])
+            rib_neighbor[ip] = [delay,sequence_number]
             start += 8*entry
 
     return src_mac,dst_mac,eth_type,src_ip,dst_ip,dsdv_type,rib_neighbor
